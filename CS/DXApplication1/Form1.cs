3c0cd65e797aca37a985ab14f01eae1463430e52
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ CS/DXApplication1/Form1.cs
@@ -0,0 +1,26 @@
+﻿using DevExpress.XtraBars.Ribbon;
+using DevExpress.Spreadsheet;
+
+namespace DXApplication1 {
+    public partial class Form1 : RibbonForm {
+
+        nwindDataSet dataSet;
+        nwindDataSetTableAdapters.InvoicesTableAdapter adapter;
+        IWorkbook template;
+
+        public Form1() {
+            InitializeComponent();
+
+            dataSet = new nwindDataSet();
+            adapter = new nwindDataSetTableAdapters.InvoicesTableAdapter();
+            adapter.Fill(dataSet.Invoices);
+        }
+
+        private void Form1_Load(object sender, System.EventArgs e) {
+            spreadsheetControl1.LoadDocument("Documents\\MailMergeTemplate_GroupData.xlsx");
+            template = spreadsheetControl1.Document;
+            template.MailMergeDataSource = dataSet;
+            template.MailMergeDataMember = "Invoices";
+        }
+    }
+}
---
+++ VB/DXApplication1/Form1.vb
@@ -0,0 +1,27 @@
+﻿Imports DevExpress.XtraBars.Ribbon
+Imports DevExpress.Spreadsheet
+
+Namespace DXApplication1
+    Partial Public Class Form1
+        Inherits RibbonForm
+
+        Private dataSet As nwindDataSet
+        Private adapter As nwindDataSetTableAdapters.InvoicesTableAdapter
+        Private template As IWorkbook
+
+        Public Sub New()
+            InitializeComponent()
+
+            dataSet = New nwindDataSet()
+            adapter = New nwindDataSetTableAdapters.InvoicesTableAdapter()
+            adapter.Fill(dataSet.Invoices)
+        End Sub
+
+        Private Sub Form1_Load(ByVal sender As Object, ByVal e As System.EventArgs) Handles MyBase.Load
+            spreadsheetControl1.LoadDocument("Documents\MailMergeTemplate_GroupData.xlsx")
+            template = spreadsheetControl1.Document
+            template.MailMergeDataSource = dataSet
+            template.MailMergeDataMember = "Invoices"
+        End Sub
+    End Class
+End Namespace
